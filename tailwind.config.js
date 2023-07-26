2901792ea11c572041d6a39ee8615ac1c79b5b92
abdulalimrakib <abdulalimrakib04@gmail.com>
2023-07-26 22:20:16 +0600

    2nd tailwind project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      animation: {
+        "bounce": "bounce 3s linear infinite",
+      },
+    },
+  },
+  plugins: [],
+};
